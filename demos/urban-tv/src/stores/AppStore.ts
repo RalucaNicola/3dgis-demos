8ddbd07a6bf4ea32d710156c2d71485ff44f277e
Arno Fiva <arnofiva@gmail.com>
2025-04-01 11:04:19 +0200

    Dynamically place TV depending on camera position and orientation

--- demos/urban-tv/src/stores/AppStore.ts
+++ demos/urban-tv/src/stores/AppStore.ts
@@ -5,13 +5,15 @@ import {
 } from "@arcgis/core/core/accessorSupport/decorators";
 import { whenOnce } from "@arcgis/core/core/reactiveUtils";
 import Mesh from "@arcgis/core/geometry/Mesh";
-import SpatialReference from "@arcgis/core/geometry/SpatialReference";
-import MeshComponent from "@arcgis/core/geometry/support/MeshComponent";
+import MeshMaterial from "@arcgis/core/geometry/support/MeshMaterial";
 import Graphic from "@arcgis/core/Graphic";
 import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
 import { FillSymbol3DLayer, MeshSymbol3D } from "@arcgis/core/symbols";
 import SceneStore from "./SceneStore";
 import UserStore from "./UserStore";
+
+import MeshLocalVertexSpace from "@arcgis/core/geometry/support/MeshLocalVertexSpace";
+import * as meshUtils from "@arcgis/core/geometry/support/meshUtils";
 
 type AppStoreProperties = Pick<AppStore, "webSceneId" | "skipPreload">;
 
@@ -81,7 +83,7 @@ class AppStore extends Accessor {
         slide.applyTo(view, { animate: false });
         await whenOnce(() => !view.updating);
       }
-      slides.getItemAt(0)?.applyTo(view, { animate: false });
+      await slides.getItemAt(0)?.applyTo(view, { animate: false });
     }
 
     this._loading = "done";
@@ -89,7 +91,7 @@ class AppStore extends Accessor {
     this.addTV();
   }
 
-  private addTV() {
+  private async addTV() {
     const video = document.createElement("video");
     video.src = "./Shot_02_1080.mp4";
     video.autoplay = false;
@@ -105,68 +107,50 @@ class AppStore extends Accessor {
     video.style.visibility = "hidden";
     document.body.append(video);
 
-    const mesh = new Mesh({
-      spatialReference: SpatialReference.WebMercator,
-      components: [
-        new MeshComponent({
-          faces: [8, 9, 10, 8, 10, 11],
-          material: {
-            colorTexture: {
-              data: video,
-            },
-            alphaMode: "auto",
-            alphaCutoff: 0.5,
-            doubleSided: true,
-          },
-          shading: "source",
-        }),
-        new MeshComponent({
-          faces: [
-            0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 12, 13, 14, 12, 14, 15, 16, 17,
-            18, 16, 18, 19, 20, 21, 22, 20, 22, 23,
-          ],
-          material: {
-            color: [40, 20, 20, 255],
-            alphaMode: "auto",
-            alphaCutoff: 0.5,
-            doubleSided: true,
-          },
-          shading: "source",
-        }),
-      ],
-      vertexSpace: {
-        type: "local",
-        origin: [950184.2042933014, 6004001.337255693, 420],
+    const camera = this.sceneStore.view.camera;
+
+    let mesh = Mesh.createBox(camera.position, {
+      imageFace: "south",
+      size: {
+        height: 18,
+        width: 32,
+        depth: 1.5,
       },
-      transform: {
-        translation: [0, 0, 0],
-        rotationAxis: [0, 0, 1],
-        rotationAngle: 250,
-        scale: [11 / 6, 1.5, 1.5],
-        // scale: [0.4773428993661809, 0.4773428993661765, 0.4773428993661756]
-      },
-      vertexAttributes: {
-        position: [
-          -8, -0.5, 0, 8, -0.5, 0, 8, -0.5, 11, -8, -0.5, 11, 8, -0.5, 0, 8,
-          0.5, 0, 8, 0.5, 11, 8, -0.5, 11, 8, 0.5, 0, -8, 0.5, 0, -8, 0.5, 11,
-          8, 0.5, 11, -8, 0.5, 0, -8, -0.5, 0, -8, -0.5, 11, -8, 0.5, 11, -8,
-          -0.5, 11, 8, -0.5, 11, 8, 0.5, 11, -8, 0.5, 11, -8, 0.5, 0, 8, 0.5, 0,
-          8, -0.5, 0, -8, -0.5, 0,
-        ],
-        uv: [
-          0, 0.625, 0.25, 0.625, 0.25, 0.375, 0, 0.375, 0.25, 0.625, 0.5, 0.625,
-          0.5, 0.375, 0.25, 0.375, 0, 1, 1, 1, 1, 0, 0, 0, 0.75, 0.625, 1,
-          0.625, 1, 0.375, 0.75, 0.375, 0, 0.375, 0.25, 0.375, 0.25, 0.125, 0,
-          0.125, 0, 0.875, 0.25, 0.875, 0.25, 0.625, 0, 0.625,
-        ],
-        normal: [
-          0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
-          0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, -1, 0, 0, -1, 0, 0, -1, 0,
-          0, -1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1,
-          0, 0, -1, 0, 0, -1,
-        ],
-      } as any,
+      vertexSpace: "local",
+      material: new MeshMaterial({
+        colorTexture: {
+          data: video,
+        },
+        alphaMode: "auto",
+        alphaCutoff: 0.5,
+        doubleSided: true,
+      }),
     });
+
+    // Slightly rotate screen
+    mesh.rotate(0, 0, -15);
+    // Move screen away from camera
+    mesh.offset(0, 170, -10);
+
+    // Define origin of screen to be camera position (again)
+    mesh = await meshUtils.convertVertexSpace(
+      mesh,
+      new MeshLocalVertexSpace({
+        origin: [camera.position.x, camera.position.y, camera.position.z!],
+      }),
+    );
+
+    // Rotate screen in front of camera
+    mesh.rotate(0, 0, -camera.heading);
+
+    if (mesh.components && 1 < mesh.components.length) {
+      mesh.components[1].material = new MeshMaterial({
+        color: [40, 20, 20, 255],
+        alphaMode: "auto",
+        alphaCutoff: 0.5,
+        doubleSided: true,
+      });
+    }
 
     const layer = new GraphicsLayer({
       graphics: [
